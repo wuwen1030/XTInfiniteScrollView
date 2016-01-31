ce7fca4e64176c5412391d40ad260c6290a48f57
吾问 <wuwen.xb@alibaba-inc.com>
2016-01-31 11:57:45 +0800

    增加API文档

--- XTInfiniteScrollView/XTInfiniteScrollView/XTInfiniteScrollView.h
+++ XTInfiniteScrollView/XTInfiniteScrollView/XTInfiniteScrollView.h
@@ -12,12 +12,35 @@
 
 @protocol XTInfiniteScrollViewDataSource <NSObject>
 
+/**
+ *  Asks the data source for number of images.
+ *
+ *  @param infiniteScrollView A infinite scroll view which informs the data source.
+ *
+ *  @return Number of images.
+ */
 - (NSUInteger)numberOfImagesInInfiniteScrollView:(XTInfiniteScrollView *)infiniteScrollView;
 
 @optional
+/**
+ *  Asks the data source for URL of image.
+ *
+ *  @param infiniteScrollView A infinite scroll view which informs the data source.
+ *  @param index              Index of the image view.
+ *
+ *  @return A URL of image.
+ */
 - (NSURL *)infiniteScrollView:(XTInfiniteScrollView *)infiniteScrollView
            urlForImageAtIndex:(NSUInteger)index;
 
+/**
+ *  Asks the data source for placeholder image.
+ *
+ *  @param infiniteScrollView A infinite scroll view which informs the data source.
+ *  @param index              Index of the image view.
+ *
+ *  @return A placeholder image.
+ */
 - (UIImage *)infiniteScrollView:(XTInfiniteScrollView *)infiniteScrollView
      placeholderForImageAtIndex:(NSUInteger)index;
 
@@ -26,9 +49,21 @@
 @protocol XTInfiniteScrollViewDelegate <NSObject>
 
 @optional
+/**
+ *  Notify the delegate that image was selected.
+ *
+ *  @param infiniteScrollView A infinite scroll view which informs the delegate.
+ *  @param index              Index of the image view.
+ */
 - (void)infiniteScrollView:(XTInfiniteScrollView *)infiniteScrollView
      didSelectImageAtIndex:(NSUInteger)index;
 
+/**
+ *  Notify the delegate that image is showing.
+ *
+ *  @param infiniteScrollView A infinite scroll view which informs the delegate.
+ *  @param index              Index of the image view.
+ */
 - (void)infiniteScrollView:(XTInfiniteScrollView *)infiniteScrollView
        didShowImageAtIndex:(NSUInteger)index;
 
@@ -36,11 +71,26 @@
 
 @interface XTInfiniteScrollView : UIView
 
+/**
+ *  Should auto scroll the image views, default is NO.
+ */
 @property (nonatomic, assign) BOOL shouldAutoScroll;
+/**
+ *  The auto scroll interval, default is 3, should large than 1.
+ */
 @property (nonatomic, assign) NSTimeInterval autoScrollInterval;
+/**
+ *  The data source
+ */
 @property (nonatomic, weak) id<XTInfiniteScrollViewDataSource> dataSource;
+/**
+ *  The delegate
+ */
 @property (nonatomic, weak) id<XTInfiniteScrollViewDelegate> delegate;
 
+/**
+ *  Reload data. Should be called once upon data source changed.
+ */
 - (void)reloadData;
 
 @end
